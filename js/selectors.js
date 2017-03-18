4249d85a627178f46e864f8fbcec9cdf72aed79f
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-03-18 13:58:34 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+
+
+function paragraphSelector(){
+  return $('p')
+}
+
+var lastImageSelector = () => {
+  return $('img:last')
+}
+
+var ninjaBabySelector = () => {
+  return $('#baby-ninja')
+}
+
+var divSelector = () => {
+  return $('.pics')
+}
+
+var firstListItem = () => {
+  return $('ul#pic-list li:first-child')
+}
